95b45c0496c49d14e336a724038932b0b687cde8
Vladislav Kosyachenko <cwat2011@yandex.ru>
2023-01-17 13:55:03 +0300

    Fix popup.js

--- src/script/Popup.js
+++ src/script/Popup.js
@@ -5,12 +5,12 @@ export default class Popup {
 
   open() {
     this._selectorPopup.classList.add('popup_opened');
+    document.addEventListener('keydown', this._handleEscClose)
   }
 
   close() {
     this._selectorPopup.classList.remove('popup_opened');
     document.removeEventListener('keydown', this._handleEscClose)
-    this._selectorPopup.removeEventListener('mousedown', this._handleOverlay)
   }
 
   _handleEscClose = (evt) => {
@@ -30,7 +30,7 @@ export default class Popup {
     // навешивает слушатель события при нажатии на блок с попапом, колбек должен
     // закрыть форму при клике по заднему фону
     this._selectorPopup.addEventListener('mousedown', this._handleOverlay)
-    document.addEventListener('keydown', this._handleEscClose)
+    this._selectorPopup.querySelector('.popup__close').addEventListener('mousedown', this.close);
   }
 }
 
